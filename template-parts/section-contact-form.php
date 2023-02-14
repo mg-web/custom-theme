1dea15a43601bb3949b0da30c5817825cc10be2a
DigitalBrent <blackbowrodent@gmail.com>
2023-02-14 10:22:27 -0500

    Added gravity forms section & styles

---
+++ template-parts/section-contact-form.php
@@ -0,0 +1,7 @@
+<?php
+// Quick Contact Form
+?>
+
+<section class="mg-contact">
+    <?php gravity_form( 5 ); ?>
+</section>
--- templates/style-guide.php
+++ templates/style-guide.php
@@ -720,6 +720,7 @@ get_header();
     get_template_part( 'template-parts/section-trust');
     get_template_part( 'template-parts/section-callout');
     get_template_part( 'template-parts/section-service-areas');
+    get_template_part( 'template-parts/section-contact-form');
   ?>
 
 <p style="grid-column: span 12; text-align: center;">Created by <a href="http://digitalbrent.com/">DigitalBrent</a></p>
