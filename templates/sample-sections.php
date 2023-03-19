8c6be59e5235119f529125a35e6538eaca011b71
DigitalBrent <blackbowrodent@gmail.com>
2023-03-19 11:48:12 -0400

    Assigned sections to designers in todo list.

--- templates/sample-sections.php
+++ templates/sample-sections.php
@@ -12,7 +12,7 @@ get_template_part( 'template-parts/section-reviews');
 get_template_part( 'template-parts/section-services');
 get_template_part( 'template-parts/section-txt-img');
 get_template_part( 'template-parts/section-trust');
-get_template_part( 'template-parts/section-callout');
+get_template_part( 'template-parts/section-callout'); // Full width text.
 get_template_part( 'template-parts/section-service-areas');
 get_template_part( 'template-parts/section-contact-form');
 get_template_part( 'template-parts/social-menu');
